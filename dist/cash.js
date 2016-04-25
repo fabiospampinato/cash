b73e55ff11db1e51fd69ef1e3a87f8e141e35de0
Shaw <shshaw@users.noreply.github.com>
2016-04-25 11:29:37 -0500

    Version bump

--- dist/cash.js
+++ dist/cash.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/*! cash-dom 1.2.0, https://github.com/kenwheeler/cash @license MIT */
+/*! cash-dom 1.2.1, https://github.com/kenwheeler/cash @license MIT */
 (function (root, factory) {
   if (typeof define === "function" && define.amd) {
     define(factory);
