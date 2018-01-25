858f4cfe480324cddbe7e6fb7a2a109369eb7001
Gabriel Ricard <4550801+gricard@users.noreply.github.com>
2018-01-25 07:47:51 -0500

    Added basic show/hide with display value tracking

--- dist/cash.js
+++ dist/cash.js
@@ -427,6 +427,40 @@
 
   });
 
+  var displayTrackerProp = "__cash_track_display";
+  function getElemDisplay(elem) {
+    return elem[displayTrackerProp] ? elem[displayTrackerProp] : "";
+  }
+
+  function trackElemDisplay(elem) {
+    if (elem.style.display !== "none" && !elem[displayTrackerProp]) {
+      elem[displayTrackerProp] = elem.style.display;
+    }
+  }
+
+  function showHide(els, hide) {
+    if (!els || els.length < 1) {
+      return;
+    }
+    var updates = [];
+    // get new display values first for elements that need an update
+    els.each(function (el) {
+      if (hide && el.style.display !== "none") {
+        trackElemDisplay(el);
+        updates.push({ el: el, display: "none" });
+      } else if (!hide && el.style.display === "none") {
+        updates.push({ el: el, display: getElemDisplay(el) });
+      }
+    });
+    if (updates.length < 1) {
+      return;
+    }
+    // now update the elements
+    updates.forEach(function (item) {
+      item.el.style.display = item.display;
+    });
+  }
+
   var camelCase = (function () {
     var camelRegex = /(?:^\w|[A-Z]|\b\w)/g, whiteSpace = /[\s-_]+/g;
     return function (str) {
@@ -475,6 +509,14 @@
       }
 
       return this;
+    },
+
+    show: function () {
+      showHide(this, false);
+    },
+
+    hide: function () {
+      showHide(this, true);
     }
 
   });
