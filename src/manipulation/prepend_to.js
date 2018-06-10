4a6359c722c8c73249727b06b40a6ebac9700d1d
Fabio Spampinato <spampinabio@gmail.com>
2018-06-10 21:58:11 +0200

    $.fn.after|prependTo: avoiding mutating passed objects

--- src/manipulation/prepend_to.js
+++ src/manipulation/prepend_to.js
@@ -1,9 +1,10 @@
 
 // @require core/cash.js
 // @require core/variables.js
+// @require collection/slice.js
 // @require ./helpers/insert_content.js
 
 fn.prependTo = function ( parent ) {
-  insertContent ( cash ( parent ), reverse.apply ( this ), true );
+  insertContent ( cash ( parent ), reverse.apply ( this.slice () ), true );
   return this;
 };
