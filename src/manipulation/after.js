5793fd31a3f64c31b9d008ac345ab755892a9780
Fabio Spampinato <spampinabio@gmail.com>
2018-06-10 19:42:51 +0200

    $.fn.after|prependTo: fixed ordering

--- src/manipulation/after.js
+++ src/manipulation/after.js
@@ -1,11 +1,12 @@
 
 // @require core/cash.js
 // @require core/each.js
+// @require core/variables.js
 // @require ./insert_after.js
 
 fn.after = function () {
-  each ( arguments, content => {
-    cash ( content ).insertAfter ( this );
+  each ( reverse.apply ( arguments ), content => {
+    reverse.apply ( cash ( content ) ).insertAfter ( this );
   });
   return this;
 };
--- src/manipulation/prepend_to.js
+++ src/manipulation/prepend_to.js
@@ -1,8 +1,9 @@
 
 // @require core/cash.js
+// @require core/variables.js
 // @require ./helpers/insert_content.js
 
 fn.prependTo = function ( parent ) {
-  insertContent ( cash ( parent ), this, true );
+  insertContent ( cash ( parent ), reverse.apply ( this ), true );
   return this;
 };
