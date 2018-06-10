2af959225ae13b7ff47b26487fdfa2ea28447f37
Fabio Spampinato <spampinabio@gmail.com>
2018-06-10 16:12:13 +0200

    computeStyle: ensuring only element nodes are considered

--- src/css/helpers/compute_style.js
+++ src/css/helpers/compute_style.js
@@ -2,6 +2,7 @@
 // @require core/variables.js
 
 function computeStyle ( ele, prop ) {
+  if ( ele.nodeType !== 1 ) return;
   const style = win.getComputedStyle ( ele, null );
   return prop ? style[prop] : style;
 }
