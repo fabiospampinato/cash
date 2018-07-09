0a2e075899744e3da8bb5c7d739431ecd450fbb1
Fabio Spampinato <spampinabio@gmail.com>
2018-07-09 22:01:41 +0200

    $.fn.val: ensuring setting multiple values on a `select[multiple]` works

--- src/forms/val.js
+++ src/forms/val.js
@@ -1,5 +1,7 @@
 
 // @require core/cash.js
+// @require core/each.js
+// @require core/type_checking.js
 // @require collection/each.js
 // @require ./helpers/get_value.js
 
@@ -7,6 +9,22 @@ fn.val = function ( value ) {
 
   if ( value === undefined ) return this[0] && getValue ( this[0] );
 
-  return this.each ( ( i, ele ) => { ele.value = value } ); //TODO: Does it work for select[multiple] too?
+  return this.each ( ( i, ele ) => {
+
+    if ( selectMultipleRe.test ( ele.type ) && isArray ( value ) ) {
+
+      each ( ele.options, option => {
+
+        option.selected = value.indexOf ( option.value ) >= 0;
+
+      });
+
+    } else {
+
+      ele.value = value;
+
+    }
+
+  });
 
 };
