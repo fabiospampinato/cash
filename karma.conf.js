f94dab7788d9ab0bb2b85ee5048c328522fbcdd2
Limon Monte <limon.monte@gmail.com>
2018-10-12 18:21:59 +0300

    Added cross-platform tests with SauceLabs

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,60 @@
 
+/* SAUCE LABS LAUNCHERS */
+
+const isSauceLabs = process.argv.includes ( '--sauce' );
+
+const SauceLabsLaunchers = {
+  sauce_ie_10: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    version: '10.0',
+    platform: 'Windows 7'
+  },
+  sauce_ie_11: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    version: '11.103',
+    platform: 'Windows 10'
+  },
+  sauce_edge_15: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    version: '15.15063'
+  },
+  sauce_edge_17: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    version: '17.17134'
+  },
+  sauce_safari: {
+    base: 'SauceLabs',
+    browserName: 'Safari',
+    version: '11.1',
+    platform: 'macOS 10.13'
+  },
+  sauce_iphone: {
+    base: 'SauceLabs',
+    browserName: 'Safari',
+    deviceName: 'iPhone XS Simulator',
+    platformName: 'iOS',
+    platformVersion: '12.0'
+  },
+  sauce_android_kitkat: {
+    base: 'SauceLabs',
+    deviceName: 'Android Emulator',
+    browserName: 'Browser',
+    platformVersion: '4.4',
+    platformName: 'Android'
+  },
+  sauce_android_marshmallow: {
+    base: 'SauceLabs',
+    deviceName: 'Android Emulator',
+    browserName: 'Chrome',
+    platformVersion: '6.0',
+    platformName: 'Android'
+  }
+};
+
 /* CONFIG */
 
 function config ( config ) {
@@ -10,18 +66,21 @@ function config ( config ) {
     plugins: [
       'karma-qunit',
       'karma-chrome-launcher',
+      'karma-sauce-launcher',
       'karma-spec-reporter'
     ],
     files: [
       'dist/cash.js',
       'test/index.js'
     ],
-    browsers: [
-      'Chrome'
-    ],
+    browsers: isSauceLabs ? Object.keys ( SauceLabsLaunchers ) : ['Chrome'],
+    customLaunchers: SauceLabsLaunchers,
     reporters: [
-      'spec'
-    ]
+      'spec',
+      'saucelabs'
+    ],
+    captureTimeout: 360000,
+    browserNoActivityTimeout: 360000
   });
 
 }
