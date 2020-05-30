760a388231063d5f1718094764c8957d19d56d88
Fabio Spampinato <spampinabio@gmail.com>
2020-05-31 00:59:37 +0100

    Updated “EleLoose” type

--- src/core/types.ts
+++ src/core/types.ts
@@ -23,7 +23,7 @@ interface CashStatic {
 type falsy = undefined | null | false | 0 | '';
 
 type Ele = Window | Document | HTMLElement | Element | Node;
-type EleLoose = Window & Document & HTMLElement & Element & Node; //UGLY: Trick to remove some kind-of useless type errors //URL: https://github.com/fabiospampinato/cash/issues/278
+type EleLoose = HTMLElement & Element & Node; //UGLY: Trick to remove some kind-of useless type errors //URL: https://github.com/fabiospampinato/cash/issues/278
 type Selector = falsy | string | Function | HTMLCollection | NodeList | Ele | Ele[] | ArrayLike<Ele> | Cash;
 type Comparator = string | Ele | Cash | (( this: EleLoose, index: number, ele: EleLoose ) => boolean);
 type Context = Document | HTMLElement | Element;
