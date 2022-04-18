a287a8648e4424d14c6d4a39c7618dca01794505
Jason Go <jasongo@jasongo.net>
2022-04-19 04:30:57 +0800

    Add regex to read scientific notation values as string and not parse it as number.

--- src/data/helpers/get_data.ts
+++ src/data/helpers/get_data.ts
@@ -2,13 +2,14 @@
 // @require core/attempt.ts
 // @require core/camel_case.ts
 
-const JSONStringRe = /^\s+|\s+$/;
+const trailingWhitespace = /^\s+|\s+$/;
+const scientificNotation = /e[+-]?\d+$/;
 
 function getData ( ele: EleLoose, key: string ): any {
 
   const value = ele.dataset[key] || ele.dataset[camelCase ( key )];
 
-  if ( JSONStringRe.test ( value ) ) return value;
+  if ( trailingWhitespace.test ( value ) || scientificNotation.test ( value ) ) return value;
 
   return attempt ( JSON.parse, value );
 
