9ce28b26b5ee7f5c7f4164ab7dddff9fa4dd64a2
Vova Yatsyuk <vovayatsyuk@users.noreply.github.com>
2022-11-19 00:58:52 +0200

    Ensuring selectors contains escaped dots are handled correctly
    Fixes https://github.com/fabiospampinato/cash/issues/369

--- src/core/find.ts
+++ src/core/find.ts
@@ -8,7 +8,7 @@ function find ( selector: string, context: Ele ): ArrayLike<Element> {
   return !selector || ( !isFragment && !isDocument ( context ) && !isElement ( context ) )
            ? []
            : !isFragment && classRe.test ( selector )
-             ? context.getElementsByClassName ( selector.slice ( 1 ) )
+             ? context.getElementsByClassName ( selector.slice ( 1 ).replace ( /\\/g, '' ) )
              : !isFragment && tagRe.test ( selector )
                ? context.getElementsByTagName ( selector )
                : context.querySelectorAll ( selector );
